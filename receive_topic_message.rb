7fe6604f6f3e1b43ad79035617a3522a43ad540f
Andrew Klotz <agc.klotz@gmail.com>
2017-06-04 14:37:42 -0400

    add dead letter queue

--- receive_topic_message.rb
+++ receive_topic_message.rb
@@ -13,25 +13,29 @@ channel.prefetch(10)
 # create exchange
 exchange = channel.topic('rlto_topic_exchange')
 
-dlx  = channel.fanout("bunny.examples.dlx.exchange")
-dlq  = channel.queue("", :exclusive => true).bind(dlx)
+dl_exchange = channel.fanout("bunny.examples.dl_exchange")
+dl_queue    = channel.queue("", :exclusive => true).bind(dl_exchange)
 
 # create queue
 queue_name = ARGV[0]
-q = channel.queue("rlto_queue_#{queue_name}", durable: true, arguments: {"x-dead-letter-exchange" => dlx.name})
-q.bind(exchange, routing_key: queue_name)
+queue = channel.queue(
+  "rlto_queue_#{queue_name}",
+  durable:   true,
+  arguments: {"x-dead-letter-exchange" => dl_exchange.name}
+)
+queue.bind(exchange, routing_key: queue_name)
 
 puts ' [*] Waiting for logs. To exit press CTRL+C'
 
 begin
-  q.subscribe(block: true, manual_ack: true) do |delivery_info, properties, body|
+  queue.subscribe(block: true, manual_ack: true) do |delivery_info, properties, body|
     if false
       channel.acknowledge(delivery_info.delivery_tag, false)
       puts " [x] Received #{delivery_info.routing_key}:#{body}"
     else
       channel.reject(delivery_info.delivery_tag, false)
       puts " [x] Rejected #{delivery_info.routing_key}:#{body}"
-      puts dlq.message_count
+      puts dl_queue.message_count
     end
   end
 rescue Interrupt => _
