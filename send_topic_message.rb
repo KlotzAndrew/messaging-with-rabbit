18b7e0d1b6483a4de26f0aa7d0ae1720c53f4564
Andrew Klotz <agc.klotz@gmail.com>
2017-06-03 23:14:04 -0400

    include publisher confirms

--- send_topic_message.rb
+++ send_topic_message.rb
@@ -1,23 +1,30 @@
 require 'bunny'
 
-abort "Usage: #{$0} [routing_key, message]" if ARGV.empty?
+abort "Usage: #{$PROGRAM_NAME} [routing_key, message]" if ARGV.empty?
 
 # open a connection
-conn = Bunny.new(host: "192.168.99.100")
+conn = Bunny.new(host: '192.168.99.100')
 conn.start
 
 # open a channel
 ch = conn.create_channel
+ch.confirm_select
 
 # create exchange
-x = ch.topic("rlto_topic_exchange")
+x = ch.topic('rlto_topic_exchange')
+
+# get message arguments
+routing_key = ARGV.shift
+msg         = ARGV.empty? ? 'Hello RLTO!' : ARGV.join(' ')
 
 # publish message
-routing_key = ARGV.shift
-msg         = ARGV.empty? ? "Hello RLTO!" : ARGV.join(" ")
-x.publish(msg, :routing_key => routing_key)
+x.publish(msg, routing_key: routing_key)
+if x.wait_for_confirms
+  puts " [x] Sent #{routing_key}:#{msg}"
+else
+  puts " [x] Failed #{routing_key}:#{msg}"
+end
 
-puts " [x] Sent #{routing_key}:#{msg}"
-
+# cleanup
 ch.close
 conn.close
