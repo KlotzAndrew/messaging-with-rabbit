77aa4dbb987d32b3fdccee3fc3de9480594b2e73
Andrew Klotz <agc.klotz@gmail.com>
2017-06-04 13:19:01 -0400

    more descritive variable names

--- receive_topic_message.rb
+++ receive_topic_message.rb
@@ -3,28 +3,28 @@ require 'bunny'
 abort "Usage: #{$PROGRAM_NAME} [binding_key]" if ARGV.empty?
 
 # open a connection
-conn = Bunny.new(host: '192.168.99.100')
-conn.start
+connection = Bunny.new(host: '192.168.99.100')
+connection.start
 
 # open a channel
-ch = conn.create_channel
+channel = connection.create_channel
 
 # create exchange
-x = ch.topic('rlto_topic_exchange')
+exchange = channel.topic('rlto_topic_exchange')
 
 # create queue
 queue_name = ARGV[0]
-q = ch.queue("rlto_queue_#{queue_name}", durable: true)
-q.bind(x, routing_key: queue_name)
+q = channel.queue("rlto_queue_#{queue_name}", durable: true)
+q.bind(exchange, routing_key: queue_name)
 
 puts ' [*] Waiting for logs. To exit press CTRL+C'
 
 begin
   q.subscribe(block: true, manual_ack: true) do |delivery_info, properties, body|
-    ch.acknowledge(delivery_info.delivery_tag, false)
+    channel.acknowledge(delivery_info.delivery_tag, false)
     puts " [x] #{delivery_info.routing_key}:#{body}"
   end
 rescue Interrupt => _
-  ch.close
-  conn.close
+  channel.close
+  connection.close
 end
--- send_topic_message.rb
+++ send_topic_message.rb
@@ -2,29 +2,29 @@ require 'bunny'
 
 abort "Usage: #{$PROGRAM_NAME} [routing_key, message]" if ARGV.empty?
 
-# open a connection
-conn = Bunny.new(host: '192.168.99.100')
-conn.start
+# open a connectionection
+connection = Bunny.new(host: '192.168.99.100')
+connection.start
 
 # open a channel
-ch = conn.create_channel
-ch.confirm_select
+channel = connection.create_channel
+channel.confirm_select
 
 # create exchange
-x = ch.topic('rlto_topic_exchange')
+exchange = channel.topic('rlto_topic_exchange')
 
 # get message arguments
 routing_key = ARGV.shift
 msg         = ARGV.empty? ? 'Hello RLTO!' : ARGV.join(' ')
 
 # publish message
-x.publish(msg, routing_key: routing_key)
-if x.wait_for_confirms
+exchange.publish(msg, routing_key: routing_key)
+if exchange.wait_for_confirms
   puts " [x] Sent #{routing_key}:#{msg}"
 else
   puts " [x] Failed #{routing_key}:#{msg}"
 end
 
 # cleanup
-ch.close
-conn.close
+channel.close
+connection.close
