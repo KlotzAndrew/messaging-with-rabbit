c6875a7d1a9ba7540ea9152f74674b6f4ac72c49
Andrew Klotz <agc.klotz@gmail.com>
2017-06-04 14:37:15 -0400

    add dead letter queuegs

--- receive_topic_message.rb
+++ receive_topic_message.rb
@@ -8,21 +8,31 @@ connection.start
 
 # open a channel
 channel = connection.create_channel
+channel.prefetch(10)
 
 # create exchange
 exchange = channel.topic('rlto_topic_exchange')
 
+dlx  = channel.fanout("bunny.examples.dlx.exchange")
+dlq  = channel.queue("", :exclusive => true).bind(dlx)
+
 # create queue
 queue_name = ARGV[0]
-q = channel.queue("rlto_queue_#{queue_name}", durable: true)
+q = channel.queue("rlto_queue_#{queue_name}", durable: true, arguments: {"x-dead-letter-exchange" => dlx.name})
 q.bind(exchange, routing_key: queue_name)
 
 puts ' [*] Waiting for logs. To exit press CTRL+C'
 
 begin
   q.subscribe(block: true, manual_ack: true) do |delivery_info, properties, body|
-    channel.acknowledge(delivery_info.delivery_tag, false)
-    puts " [x] #{delivery_info.routing_key}:#{body}"
+    if false
+      channel.acknowledge(delivery_info.delivery_tag, false)
+      puts " [x] Received #{delivery_info.routing_key}:#{body}"
+    else
+      channel.reject(delivery_info.delivery_tag, false)
+      puts " [x] Rejected #{delivery_info.routing_key}:#{body}"
+      puts dlq.message_count
+    end
   end
 rescue Interrupt => _
   channel.close
